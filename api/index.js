12c5821230d5b3d4f963de88345d43c80d2191bf
gdebesaitis <guilhermedbdev@gmail.com>
2025-11-13 11:40:34 -0300

    feat: configs pra subir na vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import app from '../app.js'
 
-app.listen(3001, () => {
-  console.log('Server rodando na porta 3001')
-})
+// app.listen(3001, () => {
+//   console.log('Server rodando na porta 3001')
+// })
 
 export default app;
